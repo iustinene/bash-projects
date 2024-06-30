ed09ade6b170d3f582378058e93c9c37e4e538b3
iustinene <63235923+iustinene@users.noreply.github.com>
2024-06-30 14:51:56 +0300

    Update proxy.sh
    Corected names

--- proxy.sh
+++ proxy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # System proxy:
-proxy=proxy.just.ro:8080
+proxy=proxy.example.com:8080
 [ "$proxy" = DIRECT ] || export http_proxy="http://$proxy" https_proxy="http://$proxy" ftp_proxy="http://$proxy"
 
 # Proxy exceptions:
